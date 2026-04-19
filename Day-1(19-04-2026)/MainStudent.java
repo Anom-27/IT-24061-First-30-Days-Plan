56b075575748b380fe4f9715dc9ae6805603a6f0
Pritom Sen Anom <sen809894@gmail.com>
2026-04-20 02:30:43 +0600

    Add files via upload

---
+++ Day-1(19-04-2026)/MainStudent.java
@@ -0,0 +1,22 @@
+public class MainStudent{
+    static void main(String[] args){
+        Student s1=new Student();
+        s1.name="Pritom Kumar Sen Anom";
+        s1.Roll=61;
+        s1.district="Tangail";
+        s1.Number="01997931225";
+        s1.Show();
+        Student s2=new Student();
+        s2.name="Hasin Siam";
+        s2.Roll=21;
+        s2.district="Jamalpur";
+        s2.Number="01629279879";
+        s2.Show();
+        Student s3=new Student();
+        s3.name="Mahathir Mahi";
+        s3.Roll=29;
+        s3.district="Dhaka";
+        s3.Number="01749123786";
+        s3.Show();
+    }
+}

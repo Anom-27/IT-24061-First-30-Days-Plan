99dc405c2300dbab64551ed6c284fd6d1b7d1a53
Pritom Sen Anom <sen809894@gmail.com>
2026-05-03 19:18:51 +0600

    Update Example1.java

--- Day-7 (25-04-2026)/Example1.java
+++ Day-7 (25-04-2026)/Example1.java
@@ -1,25 +1,17 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+class Outer {
+    int x = 10;
+
+    class Inner {
+        void show() {
+            System.out.println(x);
+        }
+    }
+}
 
 public class Example1 {
-    public static void main(String[] args){
-
-        //Creating a List
-        List<String> fruits = new ArrayList<>();
-        fruits.add("Apple");
-        fruits.add("Banana");
-        fruits.add("Cherry");
-        System.out.println("Fruits "+ fruits);
-        String firstFruit = fruits.get(0);
-        System.out.println("First Fruit " + firstFruit);
-
-        LinkedList <String> animals= new LinkedList<>();
-        animals.add("Dog");
-        animals.add("Cat");
-        animals.add("Elephant");
-
-        System.out.println("Animals: " + animals);
-
+    public static void main(String[] args) {
+        Outer o = new Outer();
+        Outer.Inner i = o.new Inner();
+        i.show();
     }
 }

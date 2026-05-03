11f146bda3aa6365438c73ef8de2fa81bf7b7c04
Pritom Sen Anom <sen809894@gmail.com>
2026-05-03 08:50:06 +0600

    Update Example2.java

--- Day-16 (04-05-2026)/Example2.java
+++ Day-16 (04-05-2026)/Example2.java
@@ -1,16 +1,15 @@
-import java.io.FileOutputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 
-public class Example1 {
+public class Example2 {
     public static void main(String[] args) throws IOException {
-        FileOutputStream out = new FileOutputStream("output.txt");
+        FileInputStream in = new FileInputStream("output.txt");
 
-        String data = "Hello Java FileOutputStream";
-        byte[] arr = data.getBytes();
+        int data;
+        while ((data = in.read()) != -1) {
+            System.out.print((char) data);
+        }
 
-        out.write(arr);
-        out.close();
-
-        System.out.println("Data written to file");
+        in.close();
     }
 }

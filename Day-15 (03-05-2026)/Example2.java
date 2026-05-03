e4901c98827bf175600f97b869ff4609f1ec6233
Pritom Sen Anom <sen809894@gmail.com>
2026-05-03 08:45:10 +0600

    Update Example2.java

--- Day-15 (03-05-2026)/Example2.java
+++ Day-15 (03-05-2026)/Example2.java
@@ -1,9 +1,18 @@
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.FileWriter;
+import java.io.File;
 
 public class Example2 {
     public static void main(String[] args) throws IOException {
+        File inputFile = new File("input.txt");
+        if (!inputFile.exists()) {
+            FileWriter writer = new FileWriter(inputFile);
+            writer.write("Hello, this is input file!");
+            writer.close();
+        }
+
         FileInputStream in = new FileInputStream("input.txt");
         FileOutputStream out = new FileOutputStream("copy.txt");
 
@@ -15,6 +24,6 @@ public class Example2 {
         in.close();
         out.close();
 
-        System.out.println("File copied");
+        System.out.println("File copied successfully!");
     }
 }
